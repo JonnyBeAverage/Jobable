9fb3b9b004da425fdd47d474d67b26866061e914
Isaac Shane <143560342+ishane0620@users.noreply.github.com>
2026-03-03 13:57:41 +0900

    added encoder (transformer) scorer and tests

--- jobable/ml_logic/matching.py
+++ jobable/ml_logic/matching.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from preprocess import preprocess_text
+from sentence_transformers import SentenceTransformer, util
+
 def compute_similarity(text1, text2):
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform([text1, text2])
@@ -11,9 +13,9 @@ def compute_similarity(text1, text2):
 
 def count_matching_keywords_no_repeats(resume_text, job_text):
     """
-    job (Series), resume (Series): counts number of overlapping data keywords
-    count_score(job, resume): counts repeats
-    count_score(resume, job): doesn’t counts repeats
+    job_text (str), resume_text (str): (order of inputs doesnt matter)
+    counts number of overlapping unique data keywords
+
     """
 
     bag_of_resume = preprocess_text(resume_text) ##gets list of key words
@@ -26,3 +28,20 @@ def count_matching_keywords_no_repeats(resume_text, job_text):
             matching.add(key_word)
 
     return len(key_word)
+
+
+def encoder_scoring(resume_text, job_text):
+    '''
+    transformer model encodes both texts into a 1D tensor/vectors
+    and finds consine similarity of the two tensors
+    '''
+
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+    resume_emb = model.encode(resume_text, convert_to_tensor=True)
+    job_embs = model.encode(job_text, convert_to_tensor=True)
+
+    sim = util.cos_sim(resume_emb, job_embs)[0].cpu().numpy()
+
+    return sim
--- jobable/ml_logic/test_pipeline.py
+++ jobable/ml_logic/test_pipeline.py
@@ -1,7 +1,7 @@
 from load_data import load_data
 from preprocess import add_bag_of_words_column
 from frequency import get_wordcounts
-from matching import compute_similarity, count_matching_keywords_no_repeats
+from matching import compute_similarity, count_matching_keywords_no_repeats, encoder_scoring
 from recommendation import recommend_skills
 
 def run_pipeline():
@@ -32,20 +32,32 @@ def run_pipeline():
     recommendations = recommend_skills(cv_skills, jd_skills, freq_dict)
     print("Recommended skills:", recommendations)
 
-def test_count_score_no_repeats():
-    # job_df = load_data("/Users/jonny/code/JonnyBeAverage/Jobable/data/Jobs.csv")
-    job_df = load_data('/Users/isaac/code/ishane0620/jobable/data/job_title_des.csv')
-    resume_df = load_data('/Users/isaac/code/ishane0620/jobable/data/Resume.csv')
 
-    test_job_instance = job_df.iloc[9, 2] #change first number to change resume instance (e.g [10,2] for the resume in index 10)
-    test_resume_instance = resume_df.iloc[5,1] #change first number to change resume instance (e.g [10,1] for the resume in index 10)
 
-    # test_resume_instance = resume_df[resume_df['Category']=='ENGINEERING'].iloc[5,1]
-    #use category=engineering to get resumes with datascience keywords
+###
+### REAL DATA TESTS
+###
 
-    return count_matching_keywords_no_repeats(test_job_instance, test_resume_instance)
+# load data
+job_df = load_data('/Users/isaac/code/ishane0620/jobable/data/job_title_des.csv')
+resume_df = load_data('/Users/isaac/code/ishane0620/jobable/data/Resume.csv')
+
+#select resume and job instance from df
+test_job_instance = job_df.iloc[9, 2] #change first number to change resume instance (e.g [10,2] for the resume in index 10)
+
+test_resume_instance = resume_df.iloc[5,1] #change first number to change resume instance (e.g [10,1] for the resume in index 10)
+# test_resume_instance = resume_df[resume_df['Category']=='ENGINEERING'].iloc[5,1]
+#use category=engineering to get resumes with datascience keywords
+
+
+def test_count_matching_keywords_no_repeats(test_job= test_job_instance,test_resume=test_resume_instance):
+    return count_matching_keywords_no_repeats(test_job, test_resume)
+
+def test_encoder_scoring(test_job= test_job_instance,test_resume=test_resume_instance):
+    return encoder_scoring(test_job, test_resume)
 
 
 if __name__ == "__main__":
     print('running')
-    print(test_count_score_no_repeats())
+    print(test_count_matching_keywords_no_repeats())
+    print(test_encoder_scoring())
