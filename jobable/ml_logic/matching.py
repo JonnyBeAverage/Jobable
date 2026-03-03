fce365a031d6099d442ad9a2cff6b61f07c743d2
Isaac Shane <143560342+ishane0620@users.noreply.github.com>
2026-03-03 13:21:17 +0900

    added count_matching keywords no repaeats test

--- jobable/ml_logic/matching.py
+++ jobable/ml_logic/matching.py
@@ -1,8 +1,28 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+from preprocess import preprocess_text
 def compute_similarity(text1, text2):
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform([text1, text2])
     score = cosine_similarity(vectors[0:1], vectors[1:2])
     return float(score[0][0])
+
+
+
+def count_matching_keywords_no_repeats(resume_text, job_text):
+    """
+    job (Series), resume (Series): counts number of overlapping data keywords
+    count_score(job, resume): counts repeats
+    count_score(resume, job): doesn’t counts repeats
+    """
+
+    bag_of_resume = preprocess_text(resume_text) ##gets list of key words
+    bag_of_job = preprocess_text(job_text) ##gets list of key words
+
+    matching = set()
+
+    for key_word in bag_of_job:
+        if key_word in bag_of_resume:
+            matching.add(key_word)
+
+    return len(key_word)
--- jobable/ml_logic/test_pipeline.py
+++ jobable/ml_logic/test_pipeline.py
@@ -1,7 +1,7 @@
 from load_data import load_data
 from preprocess import add_bag_of_words_column
 from frequency import get_wordcounts
-from matching import compute_similarity
+from matching import compute_similarity, count_matching_keywords_no_repeats
 from recommendation import recommend_skills
 
 def run_pipeline():
@@ -32,5 +32,20 @@ def run_pipeline():
     recommendations = recommend_skills(cv_skills, jd_skills, freq_dict)
     print("Recommended skills:", recommendations)
 
+def test_count_score_no_repeats():
+    # job_df = load_data("/Users/jonny/code/JonnyBeAverage/Jobable/data/Jobs.csv")
+    job_df = load_data('/Users/isaac/code/ishane0620/jobable/data/job_title_des.csv')
+    resume_df = load_data('/Users/isaac/code/ishane0620/jobable/data/Resume.csv')
+
+    test_job_instance = job_df.iloc[9, 2] #change first number to change resume instance (e.g [10,2] for the resume in index 10)
+    test_resume_instance = resume_df.iloc[5,1] #change first number to change resume instance (e.g [10,1] for the resume in index 10)
+
+    # test_resume_instance = resume_df[resume_df['Category']=='ENGINEERING'].iloc[5,1]
+    #use category=engineering to get resumes with datascience keywords
+
+    return count_matching_keywords_no_repeats(test_job_instance, test_resume_instance)
+
+
 if __name__ == "__main__":
-    run_pipeline()
+    print('running')
+    print(test_count_score_no_repeats())
