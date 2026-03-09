a66c935b0742316d90b100187342610059ab3dd4
Jonny Smith <121229956+JonnyBeAverage@users.noreply.github.com>
2026-03-09 16:11:43 +0900

    updated to phi-2 model for cover letters

--- jobable/ml_logic/test_cover_letter.py
+++ jobable/ml_logic/test_cover_letter.py
@@ -4,12 +4,12 @@ from jobable.ml_logic.cover_letter import create_cover_letter
 def run_cover_letter_test():
 
     #Load data and get 1 cv and 1 job descriptiom
-    jd_df = load_data("/Users/jonny/code/JonnyBeAverage/Jobable/data/Jobs.csv")
-    resume_df = load_data("/Users/jonny/code/JonnyBeAverage/Jobable/data/Resume.csv")
+    jd_df = load_data("jobable/data/job_title_des.csv")
+    resume_df = load_data("jobable/data/Resume.csv")
     print("Data loaded:", jd_df.shape, resume_df.shape)
 
-    single_jd_description = jd_df.iloc[0]['description']
-    single_resume_description = resume_df.iloc[0]['Resume_str']
+    single_jd_description = jd_df.sample(1)['Job Description'].iloc[0]
+    single_resume_description = resume_df.sample(1)['Resume_str'].iloc[0]
     print("Extracted Resume and JD details")
 
     cover_letter = create_cover_letter(single_resume_description, single_jd_description)
